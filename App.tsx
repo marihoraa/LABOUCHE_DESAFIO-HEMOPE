b2247bb26c9474cfa556f3b67d405391799d0834
marihoraa <maddhora@gmail.com>
2026-05-10 16:11:07 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Rotas from './src/routes/rotas';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Rotas />
+    </NavigationContainer>
+  );
+};
+
+export default App;
